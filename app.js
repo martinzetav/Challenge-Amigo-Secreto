3332e147ca4bc43fc10ab68128e4685e9cc8d325
Martin Zalazar <martinzetav@gmail.com>
2025-01-17 17:18:46 -0300

    se creo la funcionalidad para sortear un amigo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let inputAmigo = document.getElementById("amigo");
 let listaAmigos = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
 let amigos = [];
 
 function agregarAmigo(){
@@ -9,7 +10,6 @@ function agregarAmigo(){
         return alert("Por favor, inserte un nombre.");
     }
     amigos.push(nombre);
-    console.log(amigos);
     inputAmigo.value = "";
     actualizarLista();
 }
@@ -21,4 +21,17 @@ function actualizarLista(){
         item.textContent = amigo;
         listaAmigos.appendChild(item);
     })
+}
+
+function sortearAmigo(){
+    if(amigos.length === 0){
+        return alert("No hay amigos para sortear.")
+    }
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    console.log(indiceAleatorio);
+    let amigoSorteado = amigos[indiceAleatorio];
+    resultado.innerHTML = "";
+    let item = document.createElement("li");
+    item.textContent = amigoSorteado;
+    resultado.appendChild(item);
 }
